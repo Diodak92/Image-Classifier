c10df3fbd78ae2ccb78a01e25d6a05ef9db25832
Tomasz Kowalski <tomasz.kowalski92@icloud.com>
2022-05-26 23:22:44 +0200

    Added tests for train.py script

--- test_predict_script.sh
+++ test_predict_script.sh
@@ -1,5 +1,6 @@
 # Basic tests for predict.py script
-
+# show program help
+python predict.py -h
 # basic usage
 python predict.py flower_data/test/99/image_07854.jpg densenet_e20.pth
 # check if change in number of top k is working 
